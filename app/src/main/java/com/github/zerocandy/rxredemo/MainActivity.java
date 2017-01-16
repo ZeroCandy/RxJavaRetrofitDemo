b6abd1dc75744a2469e2e21af3cc819d475edc7a
ZeroCandy <ct-zsylyt@163.com>
2017-01-16 11:03:45 +0800

    Initial commit

---
+++ app/src/main/java/com/github/zerocandy/rxredemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.github.zerocandy.rxredemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    Unbinder mUnbinder;
+
+    @BindView(R.id.tv_result)
+    TextView mResultTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mUnbinder = ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mUnbinder.unbind();
+    }
+
+    @OnClick(R.id.btn)
+    public void onClick() {
+        getMovie();
+    }
+
+    private void getMovie() {
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/github/zerocandy/rxredemo/net/MovieService.java
@@ -0,0 +1,9 @@
+package com.github.zerocandy.rxredemo.net;
+
+/**
+ * Created by 帅阳 on 2017/1/16.
+ */
+
+public interface MovieService {
+    
+}
