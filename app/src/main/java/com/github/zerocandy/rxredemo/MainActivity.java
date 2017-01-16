6d7de53fa578c699cd616de9c315cf1f4a33e6ae
ZeroCandy <ct-zsylyt@163.com>
2017-01-16 11:42:50 +0800

    添加Retrofit网络请求逻辑

--- app/src/main/java/com/github/zerocandy/rxredemo/MainActivity.java
+++ app/src/main/java/com/github/zerocandy/rxredemo/MainActivity.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.github.zerocandy.rxredemo.bean.MovieEntity;
+import com.github.zerocandy.rxredemo.net.MovieService;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,8 +43,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getMovie() {
+        String baseUrl = "https://api.douban.com/v2/movie/";
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        MovieService movieService = retrofit.create(MovieService.class);
+        Call<MovieEntity> call = movieService.getTop250Movie(0, 10);
+        call.enqueue(new Callback<MovieEntity>() {
+            @Override
+            public void onResponse(Call<MovieEntity> call, Response<MovieEntity> response) {
+                mResultTv.setText(response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<MovieEntity> call, Throwable t) {
+                mResultTv.setText(t.getMessage());
+            }
+        });
     }
 
 
--- app/src/main/java/com/github/zerocandy/rxredemo/net/MovieService.java
+++ app/src/main/java/com/github/zerocandy/rxredemo/net/MovieService.java
@@ -1,9 +1,16 @@
 package com.github.zerocandy.rxredemo.net;
 
+import com.github.zerocandy.rxredemo.bean.MovieEntity;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by 帅阳 on 2017/1/16.
  */
 
 public interface MovieService {
-    
+    @GET("top250")
+    Call<MovieEntity> getTop250Movie(@Query("start") int start, @Query("count") int count);
 }
